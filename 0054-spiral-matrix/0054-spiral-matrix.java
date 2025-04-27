11f3c9b94b33b797490b7d8e5bd4d17065f7aa01
tushargoyalbtp <41904107+tushargoyalbtp@users.noreply.github.com>
2025-04-27 12:55:37 +0530

    0054-SPIRAL-MATRIX Stats: Time: 0 ms (100%), Space: 41.6 MB (63.79%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,36 +1,44 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
-        int row = 0, col = 0;
+        int row = 0;
+        int col = 0;
         int maxRow = matrix.length - 1;
         int maxCol = matrix[0].length - 1;
+        int te = matrix.length * matrix[0].length;
+        int cntE = 0;
 
-        while (row <= maxRow && col <= maxCol) {
+        while(cntE < te){
 
-            for (int j = col; j <= maxCol; j++) {
-                ans.add(matrix[row][j]);
+            for(int j=col, i=row; j<=maxCol && cntE < te; j++){
+                ans.add(matrix[i][j]);
+                cntE++;
+
             }
+            
             row++;
-
-            for (int i = row; i <= maxRow; i++) {
-                ans.add(matrix[i][maxCol]);
+            for(int i=row, j=maxCol; i<=maxRow && cntE < te; i++){
+                ans.add(matrix[i][j]);
+                cntE++;
             }
+
             maxCol--;
-
-            if (row <= maxRow) {
-                for (int j = maxCol; j >= col; j--) {
-                    ans.add(matrix[maxRow][j]);
-                }
-                maxRow--;
+            
+            for(int j=maxCol, i=maxRow; j>=col && cntE < te; j--){
+                ans.add(matrix[i][j]);
+                cntE++;
             }
 
-            if (col <= maxCol) {
-                for (int i = maxRow; i >= row; i--) {
-                    ans.add(matrix[i][col]);
-                }
-                col++;
+            maxRow--;
+
+            for(int i=maxRow, j=col; i>=row && cntE < te; i--){
+                ans.add(matrix[i][j]);
+                cntE++;
             }
+
+            col++;
         }
+
         return ans;
     }
 }
