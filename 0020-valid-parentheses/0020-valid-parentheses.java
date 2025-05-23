229e921be9585da0305714279432ef320b38e4bc
tushargoyalbtp <41904107+tushargoyalbtp@users.noreply.github.com>
2025-05-23 15:39:28 +0530

    0020-VALID-PARENTHESES Stats: Time: 2 ms (97.21%), Space: 41.6 MB (91.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,21 @@
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (st.isEmpty()) {
-                st.push(ch);
-            } else if (!st.isEmpty() && st.peek() == '(' && ch == ')') {
-                st.pop();
-            } else if (!st.isEmpty() && st.peek() == '[' && ch == ']') {
-                st.pop();
-            } else if (!st.isEmpty() && st.peek() == '{' && ch == '}') {
-                st.pop();
-            }
-            else{
-                st.push(ch);
+    public boolean isValid(String str) {
+        Stack<Character> sc = new Stack<>();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                sc.push(ch);
+            } else {
+                if (sc.isEmpty()) return false;
+                char top = sc.pop();
+                if ((ch == ')' && top != '(') || 
+                    (ch == '}' && top != '{') || 
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
             }
         }
-
-        return st.size() > 0 ? false : true;
+        return sc.isEmpty();
     }
 }
