bc523e0e7dbeeee9e3ae70cb1d043c84d3a57183
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-06-16 23:50:41 +0530

    Time: 49 ms (5.86%), Space: 45.8 MB (5.38%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,43 +1,37 @@
 class Solution {
+    public int totaltimerequire(int[] arr, int mid, int k) {
+        int totaltime = 0;
 
-    public int findTotalHours(int[] piles, int speed){
-        int ans = 0;
-        for(int pile : piles){
-            ans += Math.ceil((double)pile / (double)speed);
+        for (int i = 0; i < arr.length; i++) {
+            totaltime += Math.ceil((double) arr[i] / (double) mid);
         }
 
+        return totaltime;
+    }
+
+    public int minEatingSpeed(int[] arr, int k) {
+
+        int low = 1;
+        int high = Integer.MIN_VALUE;
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            high = Math.max(high, arr[i]);
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int totalHours = totaltimerequire(arr, mid, k);
+
+            if (totaltimerequire(arr, mid, k) <= k) {
+                high = mid - 1;
+                ans = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
         return ans;
-    }
 
-
-    public int findMaxPiles(int[] piles){
-        int maxPiles = 0;
-
-        for(int pile : piles){
-            maxPiles = Math.max(maxPiles, pile);
-        }
-
-        return maxPiles;
-    }
-
-    public int minEatingSpeed(int[] piles, int h) {
-        int l = 1;
-
-        int r = findMaxPiles(piles);
-
-        while(l<=r){
-            int mid = (l+r)/2;
-
-            int totalHours = findTotalHours(piles, mid);
-
-            if(totalHours <= h){
-                r = mid - 1;
-            }
-            else{
-                l = mid + 1;
-            }
-        }
-
-        return l;
     }
 }
