788328037317b6b0c4d604a238bb6e288f501522
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-06-19 19:34:17 +0530

    Time: 20 ms (75.23%), Space: 50.9 MB (56.12%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,31 +1,65 @@
 class Solution {
-    public int functionHelper(String text1, String text2, int[][] dp, int index1, int index2){
-        
-        if(index1 == 0 || index2 == 0){
-            return 0;
-        }
-        if(dp[index1][index2] != -1){
-            return dp[index1][index2];
-        }
-        if(text1.charAt(index1-1) == text2.charAt(index2-1)){
-            return dp[index1][index2] = 1 + functionHelper(text1, text2, dp, index1-1, index2-1);
-        }
-        return dp[index1][index2] = Math.max(functionHelper(text1, text2, dp, index1-1, index2), 
-        functionHelper(text1, text2, dp, index1, index2-1));
-    }
+
+    //Memomization - approach
+    // public int functionHelper(String text1, String text2, int[][] dp, int index1, int index2){
+
+    //     if(index1 == 0 || index2 == 0){
+    //         return 0;
+    //     }
+    //     if(dp[index1][index2] != -1){
+    //         return dp[index1][index2];
+    //     }
+    //     if(text1.charAt(index1-1) == text2.charAt(index2-1)){
+    //         return dp[index1][index2] = 1 + functionHelper(text1, text2, dp, index1-1, index2-1);
+    //     }
+    //     return dp[index1][index2] = Math.max(functionHelper(text1, text2, dp, index1-1, index2), 
+    //     functionHelper(text1, text2, dp, index1, index2-1));
+    // }
     public int longestCommonSubsequence(String text1, String text2) {
+        // memoization-appproach
+        // int n = text1.length();
+        // int m = text2.length();
+
+        // int[][] dp = new int[n+1][m+1];
+
+        // for(int i = 0; i < n+1; i++){
+        //     for(int j = 0; j < m+1; j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+
+        // return functionHelper(text1, text2, dp, n, m);
+
+        // tabulation-approach
         int n = text1.length();
         int m = text2.length();
 
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[n + 1][m + 1];
 
-        for(int i = 0; i < n+1; i++){
-            for(int j = 0; j < m+1; j++){
-                dp[i][j] = -1;
+        for (int i = 0; i < n + 1; i++) {
+
+            dp[i][0] = 0;
+
+        }
+        for (int j = 0; j < m + 1; j++) {
+
+            dp[0][j] = 0;
+
+        }
+
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
 
-        return functionHelper(text1, text2, dp, n, m);
+        return dp[n][m];
 
         // int count = 0;
         // HashMap<Character, Integer> charmap = new HashMap<>();
