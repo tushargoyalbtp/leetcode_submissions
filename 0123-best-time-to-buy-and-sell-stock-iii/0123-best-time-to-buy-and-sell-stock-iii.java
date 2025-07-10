918aafb167be5035aae441266d063a3f8d925484
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-07-11 01:07:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    public int functionHelper(int[][][] dp, int ind, int canBuy, int cap, int[] prices){
+        if(ind == prices.length || cap == 0){
+            return 0;
+        }
+        if(dp[ind][canBuy][cap] != 0){
+            return dp[ind][canBuy][cap];
+        }
+        int profit = 0;
+
+        if(canBuy==1){
+
+            //buy
+            int buy = -prices[ind] + functionHelper(dp, ind+1, 0, cap, prices);
+
+            // skip
+            int skip = 0 + functionHelper(dp, ind+1, 1, cap, prices);
+
+            profit = Math.max(buy, skip);
+        }
+        else{
+            //sell
+            int sell = prices[ind] + functionHelper(dp, ind+1, 1, cap-1, prices);
+
+            // skip
+            int skip = 0 + functionHelper(dp, ind+1, 0, cap, prices);
+
+            profit = Math.max(sell, skip);
+        }
+
+        dp[ind][canBuy][cap] = profit;
+        return profit;
+    }
+    public int maxProfit(int[] prices) {
+        
+        int n = prices.length;
+
+        int[][][] dp = new int[n][2][3];
+
+        return functionHelper(dp, 0, 1, 2, prices);
+    }
+}
