524c40bee486f94413ddb1ec60fb4ba1ed1ef0bb
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-07-12 20:27:43 +0530

    Time: 4 ms (52.44%), Space: 58.1 MB (5.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,23 +1,23 @@
 class Solution {
 
-    public boolean functionHelper(int i , int j, int k , int s1length, int s2length, int s3length, String s1, String s2, String s3, boolean[][][] dp){
+    public boolean functionHelper(int i , int j, int k , int s1length, int s2length, int s3length, String s1, String s2, String s3, Boolean[][][] dp){
 
         if (k == s3length) {
             return i == s1length && j == s2length;
         }
 
-        boolean ans = false;
-
-        if(dp[i][j][k] != false){
+        if (dp[i][j][k] != null) {
             return dp[i][j][k];
         }
 
-        if (i < s1.length() && s3.charAt(k) == s1.charAt(i)) {
-            ans = dp[i][j][k] || functionHelper(i + 1, j, k + 1, s1length, s2length, s3length, s1, s2, s3, dp);
+        boolean ans = false;
+
+        if (i < s1length && s3.charAt(k) == s1.charAt(i)) {
+            ans = ans || functionHelper(i + 1, j, k + 1, s1length, s2length, s3length, s1, s2, s3, dp);
         }
 
-        if (j < s2.length() && s3.charAt(k) == s2.charAt(j)) {
-            ans = dp[i][j][k] || functionHelper(i, j + 1, k + 1, s1length, s2length, s3length, s1, s2, s3, dp);
+        if (j < s2length && s3.charAt(k) == s2.charAt(j)) {
+            ans = ans || functionHelper(i, j + 1, k + 1, s1length, s2length, s3length, s1, s2, s3, dp);
         }
 
         dp[i][j][k] = ans;
@@ -25,18 +25,14 @@ class Solution {
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
         int s1length = s1.length();
         int s2length = s2.length();
         int s3length = s3.length();
 
-        boolean[][][] dp = new boolean[s1length+1][s2length+1][s3length+1];
-
         if (s1length + s2length != s3length) return false;
 
-        return functionHelper(i, j, k, s1length, s2length, s3length, s1, s2, s3, dp);
+        Boolean[][][] dp = new Boolean[s1length + 1][s2length + 1][s3length + 1];
+
+        return functionHelper(0, 0, 0, s1length, s2length, s3length, s1, s2, s3, dp);
     }
 }
