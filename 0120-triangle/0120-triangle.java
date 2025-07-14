fdc0f5e02c7ced5a12a1d379371d68298daf9956
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-07-14 22:44:19 +0530

    Time: 2 ms (71.64%), Space: 43.4 MB (96.71%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,41 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        // int n = triangle.size();
+        // // Create memoization table initialized with null (to check visited)
+        // Integer[][] dp = new Integer[n][n];
+
+        // return dfs(triangle, 0, 0, dp);
+
         int n = triangle.size();
-        // Create memoization table initialized with null (to check visited)
-        Integer[][] dp = new Integer[n][n];
+        int[] dp = new int[n];
 
-        return dfs(triangle, 0, 0, dp);
-    }
-
-    public int dfs(List<List<Integer>> triangle, int row, int col, Integer[][] dp) {
-        if (row == triangle.size() - 1) {
-            return triangle.get(row).get(col);
+        // Copy last row
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
         }
 
-        if (dp[row][col] != null) {
-            return dp[row][col]; // Return cached result
+        // Bottom-up
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
         }
-
-        int down = dfs(triangle, row + 1, col, dp);
-        int diag = dfs(triangle, row + 1, col + 1, dp);
-
-        dp[row][col] = triangle.get(row).get(col) + Math.min(down, diag);
-        return dp[row][col];
+        return dp[0];
     }
+
+    // public int dfs(List<List<Integer>> triangle, int row, int col, Integer[][] dp) {
+    //     if (row == triangle.size() - 1) {
+    //         return triangle.get(row).get(col);
+    //     }
+
+    //     if (dp[row][col] != null) {
+    //         return dp[row][col]; // Return cached result
+    //     }
+
+    //     int down = dfs(triangle, row + 1, col, dp);
+    //     int diag = dfs(triangle, row + 1, col + 1, dp);
+
+    //     dp[row][col] = triangle.get(row).get(col) + Math.min(down, diag);
+    //     return dp[row][col];
+    // }
 }
