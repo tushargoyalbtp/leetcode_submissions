09b48c530a173516aef9a0fe400bf9f0b44940be
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-07-18 14:49:09 +0530

    Time: 1 ms (62.53%), Space: 46 MB (88.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,34 +3,27 @@ class Solution {
         
         int n = height.length;
 
-        int[] left = new int[n];
+        int[] leftheight = new int[n];
 
-        int[] right = new int[n];
+        leftheight[0] = height[0];
 
-        left[0] = height[0];
-        right[n-1] = height[n-1];
+        int[] rightheight = new int[n];
+        rightheight[n-1] = height[n-1];
 
-        for(int i = 1 ; i < n; i++){
-            left[i] = Math.max(left[i-1], height[i]);
+        for(int i = 1; i < n; i++){
+            leftheight[i] = Math.max(height[i], leftheight[i-1]);
         }
 
-        for(int i = n - 2; i >= 0; i--){
-            right[i] = Math.max(height[i], right[i+1]);
+        for(int i = n-2; i>=0; i--){
+            rightheight[i] = Math.max(height[i], rightheight[i+1]);
         }
 
         int maxArea = 0;
-       for(int i=0;i<height.length;i++){
-            maxArea = maxArea  + Math.min(left[i],right[i]) - height[i];
-       }
 
-       return maxArea;
+        for(int i = 0; i < n ; i++){
+            maxArea = maxArea + Math.min(leftheight[i], rightheight[i]) - height[i];
+        }
+
+        return maxArea;
     }
 }
-
-// [0,1,0,2,1,0,1,3,2,1,2,1]
-
-// left = [0,1,1,2,2,2,2,3,3,3,3,3];
-// right = [3,3,3,3,3,3,3,3,2,2,2,1];
-
-// 0 + 0 - 0 = 0
-// 0 + 1 - 1 = 0
