7c7db0774d127b6c9e86b35f2c9f80e7441f5cde
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-08-03 10:44:46 +0530

    Time: 1 ms (92.42%), Space: 45.7 MB (36.57%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,39 +1,56 @@
-class Solution {
-    public int pivotIndex(int[] nums) {
-        int ans = 0;
-        // 0 -> i-1
-        // i+1 -> n
+// class Solution {
+//     public int pivotIndex(int[] nums) {
+//         int ans = 0;
+//         // 0 -> i-1
+//         // i+1 -> n
 
        
 
-        for(int i = 0; i < nums.length; i++){
+//         for(int i = 0; i < nums.length; i++){
 
-            int prefixSum = 0;
+//             int prefixSum = 0;
 
-            if(i>0){
-                prefixSum = sumAll(0, i-1, nums);
-            } 
+//             if(i>0){
+//                 prefixSum = sumAll(0, i-1, nums);
+//             } 
 
-            int suffixSum = 0;
+//             int suffixSum = 0;
 
-            if(i+1 < nums.length){
-                suffixSum = sumAll(i+1, nums.length-1, nums);
-            }
-            if(prefixSum == suffixSum){
-                return i;
-            }
+//             if(i+1 < nums.length){
+//                 suffixSum = sumAll(i+1, nums.length-1, nums);
+//             }
+//             if(prefixSum == suffixSum){
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+
+//     public int sumAll(int start, int end, int[] nums){
+//         int sum = 0;
+
+//         for(int i = start ; i <= end; i++){
+//             sum += nums[i];
+//         }
+
+//         return sum;
+//     }
+// }
+
+
+// optimized solution
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        int leftsum=0;
+        for(int ele:nums){
+            sum+=ele;
         }
-
+        for(int i=0;i<nums.length;i++){
+            if(leftsum==sum-leftsum-nums[i]) return i;
+            leftsum+=nums[i];
+        }
         return -1;
-    }
-
-    public int sumAll(int start, int end, int[] nums){
-        int sum = 0;
-
-        for(int i = start ; i <= end; i++){
-            sum += nums[i];
-        }
-
-        return sum;
     }
 }
