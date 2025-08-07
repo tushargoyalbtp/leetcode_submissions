52781aa88e31c153d65a40414eaeb1fff629201f
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-08-07 11:50:27 +0530

    Time: 3 ms (78.98%), Space: 45.6 MB (69.91%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,35 +1,55 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         
-        int[] ans = new int[2];
+        // int[] ans = new int[2];
 
-        HashMap<Integer,Integer> map = new HashMap<>();
+        // HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                ans[0] = nums[i];
+        // for(int i = 0; i < nums.length; i++){
+        //     if(map.containsKey(nums[i])){
+        //         ans[0] = nums[i];
+        //         break;
+        //     }
+
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        // }
+
+        // for(int i = 0; i < nums.length; i++){
+        //     if(!map.containsKey(nums[i])){
+        //         map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        //     }
+        // }
+
+
+        // for(int i = 1; i <= nums.length; i++){
+
+        //     if(!map.containsKey(i)){
+        //         ans[1] = i;
+        //         break;
+        //     }
+        // }
+
+        // return ans;
+
+       int dup = -1;
+        int missing = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]);
+            if (nums[val - 1] < 0) {
+                dup = val; // FIX: use absolute value
+            } else {
+                nums[val - 1] *= -1;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
                 break;
             }
-
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
 
-        for(int i = 0; i < nums.length; i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-            }
-        }
-
-
-        for(int i = 1; i <= nums.length; i++){
-
-            if(!map.containsKey(i)){
-                ans[1] = i;
-                break;
-            }
-        }
-
-        return ans;
-
+        return new int[]{dup, missing};
     }
 }
