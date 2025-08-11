5f9104e6f11600afc598d4f02c7d760e00887bf7
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-08-11 22:27:20 +0530

    Time: 12 ms (54.05%), Space: 42.5 MB (52.17%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,29 +1,48 @@
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         HashMap<Character, Character> mapST = new HashMap<>();
+//         HashMap<Character, Character> mapTS = new HashMap<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch1 = s.charAt(i);
+//             char ch2 = t.charAt(i);
+
+//             if (mapST.containsKey(ch1) && mapST.get(ch1) != ch2) {
+//                 return false;
+//             }
+//             if (mapTS.containsKey(ch2) && mapTS.get(ch2) != ch1) {
+//                 return false;
+//             }
+
+//             mapST.put(ch1, ch2);
+//             mapTS.put(ch2, ch1);
+//         }
+
+//         return true;
+//     }
+// }
 
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        HashMap<Character, Character> mapST = new HashMap<>();
-        HashMap<Character, Character> mapTS = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-
-            if (mapST.containsKey(ch1) && mapST.get(ch1) != ch2) {
-                return false;
+        return helper(s,t) && helper(t,s);
+    }
+    public boolean helper(String s, String t){
+        if(s.length() != t.length()) return false;
+        Map<Character,Character> map = new HashMap<>();
+        int n = s.length(),i=0;
+        while(i<n){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i)) != t.charAt(i)) return false;
+            }else{
+                map.put(s.charAt(i),t.charAt(i));
             }
-            if (mapTS.containsKey(ch2) && mapTS.get(ch2) != ch1) {
-                return false;
-            }
-
-            mapST.put(ch1, ch2);
-            mapTS.put(ch2, ch1);
+            i++;
         }
-
         return true;
     }
 }
