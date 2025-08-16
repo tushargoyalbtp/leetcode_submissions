568f015138b4912702bb593628ced55f33a1fb73
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-08-16 12:08:31 +0530

    Time: 8 ms (59.99%), Space: 42.9 MB (57.28%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,33 +1,40 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()){
+        if (s2.length() < s1.length()) {
             return false;
         }
-        int[] s1map = new int[32];
 
-        for(int i=0; i<s1.length(); i++){
-            s1map[s1.charAt(i)-'a']++;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
         }
 
-        for(int i=0; i<=s2.length()-s1.length(); i++){
-            int[] s2map = new int[26];
-            for(int j=0; j<s1.length(); j++){
-                s2map[s2.charAt(i+j)-'a']++;
-            }
-            if(matches(s1map,s2map)){
+        if (matches(s1map, s2map)) {
+            return true;
+        }
+
+        for (int i = 1; i <= s2.length() - s1.length(); i++) {
+            // remove leftmost char from window
+            s2map[s2.charAt(i - 1) - 'a']--;
+            // add new rightmost char to window
+            s2map[s2.charAt(i + s1.length() - 1) - 'a']++;
+
+            if (matches(s1map, s2map)) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean matches(int[] s1map, int[] s2map){
-        for(int i = 0; i<26; i++){
-            if(s1map[i]!=s2map[i]){
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i]) {
                 return false;
             }
         }
-
         return true;
     }
 }
