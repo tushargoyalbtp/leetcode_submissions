dd3efcff7238d6ea437ceb76b84c4ce48e941435
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-08-16 16:32:58 +0530

    Time: 9 ms (91.28%), Space: 45 MB (49.1%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+
+        List<Integer> result = new ArrayList<>();
+        
+        int m = s.length(), n = p.length();
+        
+        if(m < n) return result;
+        
+        int[] pHashTable = new int[26], sHashTable = new int[26];
+        
+        for(int i = 0; i < n; i++){
+            pHashTable[p.charAt(i) - 'a']++;
+            sHashTable[s.charAt(i) - 'a']++;
+        }
+        for(int i = n; i <= m; i++){
+            if(compare(pHashTable, sHashTable)) result.add(i - n);
+            sHashTable[s.charAt(i - n) - 'a']--;
+            if(i < m) sHashTable[s.charAt(i) - 'a']++;
+        }
+    
+        return result;
+    }
+    
+    public static boolean compare(int[] a, int[] b){
+        for(int i = 0; i < 26; i++){
+            if(a[i] != b[i]) return false;
+        }
+        return true;
+    }
+}
