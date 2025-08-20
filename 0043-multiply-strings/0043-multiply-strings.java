7f513d0c939b1da17c79755a1aa61f0214d188fb
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-08-20 09:52:43 +0530

    Time: 2 ms (98.98%), Space: 42.3 MB (64.2%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,21 +1,74 @@
-class Solution {
-public String multiply(String num1, String num2) {
-    int m = num1.length(), n = num2.length();
-    int[] pos = new int[m + n];
-   
-    for(int i = m - 1; i >= 0; i--) {
-        for(int j = n - 1; j >= 0; j--) {
-            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
-            int p1 = i + j, p2 = i + j + 1;
-            int sum = mul + pos[p2];
+// class Solution {
+//     public String multiply(String num1, String num2) {
+        
+//         int size1 = num1.length();
+        
+//         int size2 = num2.length();
 
-            pos[p1] += sum / 10;
-            pos[p2] = (sum) % 10;
+//         int[] arr1 = new int[size1];
+//         int[] arr2 = new int[size2];
+
+//         for(int i = 0 ; i < size1; i++){
+//             int digit = num1.charAt(i) - '0';
+//             arr1[i] = digit;
+//         }
+
+//         for(int i = 0 ; i < size2; i++){
+//             int digit = num2.charAt(i) - '0';
+//             arr2[i] = digit;            
+//         }
+//         int count1 = size1-1;
+        
+//         long ans1 = 0;
+
+//         for(int i = 0; i < size1; i++){
+//             ans1 = ans1 + (long)(arr1[i] * Math.pow(10,count1));
+//             count1--;    
+//         }
+
+//         int count2 = size2-1;
+        
+//         long ans2 = 0;
+
+//         for(int i = 0; i < size2; i++){
+//             ans2 = ans2 + (long)(arr2[i] * Math.pow(10,count2));
+//             count2--;    
+//         }
+
+//         return ""+(ans1 * ans2);
+//     }
+// }
+
+
+
+//optimized way
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        int m = num1.length();
+        int n = num2.length();
+        int[] result = new int[m + n];
+
+        // Multiply from right to left
+        for (int i = m - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+
+                int sum = result[i + j + 1] + d1 * d2;
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
         }
-    }  
-    
-    StringBuilder sb = new StringBuilder();
-    for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
-    return sb.length() == 0 ? "0" : sb.toString();
-}
+
+        // Build string, skipping leading zeros
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+        return sb.toString();
+    }
 }
