5d6a3d766000980f555014f1c01564bfafc7380e
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-10-13 22:23:47 +0530

    Time: 1 ms (12.53%), Space: 42.2 MB (12.86%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,29 +15,46 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null){
-            return true;
-        }
+        // if(root == null){
+        //     return true;
+        // }
 
-        return helper(root.left, root.right);
+        // return helper(root.left, root.right);
+
+        if (root == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+        return true;
     }
 
-    public boolean helper(TreeNode root1, TreeNode root2){
-        if(root1==null && root2==null){
-            return true;
-        }
-        if(root1!=null && root2 == null){
-            return false;
-        }
+    // public boolean helper(TreeNode root1, TreeNode root2){
+    //     if(root1==null && root2==null){
+    //         return true;
+    //     }
+    //     if(root1!=null && root2 == null){
+    //         return false;
+    //     }
 
-        if(root1==null && root2 != null){
-            return false;
-        }
+    //     if(root1==null && root2 != null){
+    //         return false;
+    //     }
 
 
-        if(root1.val != root2.val){
-            return false;
-        }
-        return helper(root1.left, root2.right) && helper(root1.right , root2.left);
-    }
+    //     if(root1.val != root2.val){
+    //         return false;
+    //     }
+    //     return helper(root1.left, root2.right) && helper(root1.right , root2.left);
+    // }
 }
