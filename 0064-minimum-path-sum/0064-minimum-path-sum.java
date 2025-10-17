79e73a4b382ae33e9aab637688a0adaa2842b6fd
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2025-10-17 21:22:04 +0530

    Time: 2 ms (77.28%), Space: 47.8 MB (45.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,32 +1,36 @@
 class Solution {
-    public int functionHelper(int[][] dp, int[][] grid, int n, int m, int row, int col) {
-        // If row or col goes out of bounds
-        if (row >= n || col >= m) {
-            return Integer.MAX_VALUE; // return large value, since this path is invalid
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        return helper(grid, 0, 0, m, n, dp);
+    }
+
+    private int helper(int[][] grid, int row, int col, int m, int n, int[][] dp) {
+       
+        if (row >= m || col >= n) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (row == m - 1 && col == n - 1) {
+            return grid[row][col];
         }
 
         if(dp[row][col] != -1){
             return dp[row][col];
         }
-        // If we reach the bottom-right cell
-        if (row == n - 1 && col == m - 1) {
-           return  dp[row][col] = grid[row][col];
-        }
 
-        // Choose min path: right or down
-        return dp[row][col] = grid[row][col] + Math.min(
-            functionHelper(dp, grid, n, m, row + 1, col),
-            functionHelper(dp, grid, n, m, row, col + 1)
-        );
-    }
+        int right = helper(grid, row, col + 1, m, n, dp);
+        int down = helper(grid, row + 1, col, m, n, dp);
 
-    public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
-        }
-        return functionHelper(dp, grid, n, m, 0, 0);
+
+        return dp[row][col] = grid[row][col] + Math.min(right, down);
     }
 }
