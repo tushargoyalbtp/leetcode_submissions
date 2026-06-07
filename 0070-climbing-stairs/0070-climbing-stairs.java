a085a5d09417d6bec1b41d2373e3f7ecf0402d58
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-06-07 23:21:24 +0530

    Time: 61 ms (0.27%), Space: 41.8 MB (92.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,29 +1,19 @@
 class Solution {
 
-    public int functionHelper(int n, int index, int[] dp){
 
-        if(index == n){
-            return 1;
-        }
-        if(index > n){
-            return 0;
-        }
-
-        if(dp[index] != -1){
-            return dp[index];
-        }
-
-        dp[index] = functionHelper(n, index+1, dp) + functionHelper(n, index+2, dp);
-        return dp[index];
-
-    }
     public int climbStairs(int n) {
-        
-        int[] dp = new int[n];
-        
-        Arrays.fill(dp, -1);
+        if(n<=2){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i<n+1; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
 
-        return functionHelper(n, 0, dp);
+        return dp[n];
 
     }
 }
