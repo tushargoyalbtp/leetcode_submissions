a3701f152c71bf6d339e2e9599f427b48a33d880
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-07-13 08:41:06 +0530

    Time: 0 ms (100%), Space: 44 MB (27.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,54 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        
-        int start = 0;
-        int end = nums.length - 1;
-        
-        // Find the index of the smallest element using binary search
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            
-            if (nums[mid] > nums[end]) {
-                // Pivot is in the right half
-                start = mid + 1;
-            } else {
-                // Pivot is in the left half or the array is already sorted
-                end = mid;
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } 
+            else {
+                r = mid;
             }
         }
-        
-        // At this point, 'start' is the index of the smallest element
-        int pivot = start;
-        
-        // Reset pointers for the actual search
-        start = 0;
-        end = nums.length - 1;
-        
-        // Decide which half to search based on target value
-        if (target >= nums[pivot] && target <= nums[end]) {
-            // Search in right half
-            start = pivot;
-        } else {
-            // Search in left half
-            end = pivot - 1;
+
+        int pivot = l;
+
+        l = 0;
+        r = nums.length - 1;
+
+        if(target >= nums[pivot] && target <= nums[r]){
+            l = pivot;
         }
-        
-        // Perform regular binary search
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            
-            if (nums[mid] == target) {
+        else{
+            r = pivot - 1;
+
+        }
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target){
                 return mid;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+            }
+            else if(nums[mid] < target){
+                l++;
+            }
+            else {
+                r--;
             }
         }
-        
-        return -1; // Target not found
+
+        return -1;
+
     }
 }
