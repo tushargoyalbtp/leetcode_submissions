7a7b8c90c723279e54324ccffe6f5a610e45a43e
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-07-13 07:56:06 +0530

    Time: 34 ms (61.51%), Space: 59.2 MB (45.68%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,13 +1,18 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+
         List<List<Integer>> ansList = new ArrayList<>();
+
+        Arrays.sort(nums);   
+
         int n = nums.length;
 
         for (int i = 0; i < n - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1])
+
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
+            }
 
             int l = i + 1;
             int r = n - 1;
@@ -18,18 +23,23 @@ class Solution {
                 if (sum == 0) {
                     ansList.add(Arrays.asList(nums[i], nums[l], nums[r]));
 
-                    // Skip duplicates for left and right pointers
-                    while (l < r && nums[l] == nums[l + 1])
+                    // Skip duplicates
+                    while (l < r && nums[l] == nums[l + 1]) {
                         l++;
-                    while (l < r && nums[r] == nums[r - 1])
+                    }
+
+                    while (l < r && nums[r] == nums[r - 1]) {
                         r--;
+                    }
 
                     l++;
                     r--;
-                } else if (sum < 0) {
-                    l++;
-                } else {
-                    r--;
+                }
+                else if (sum < 0) {
+                    l++;    // Fix: move left pointer right
+                }
+                else {
+                    r--;    // Fix: move right pointer left
                 }
             }
         }
