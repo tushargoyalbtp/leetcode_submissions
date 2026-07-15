cef2065b2361a927bbd283be6e591ae54fe07f28
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-07-15 23:05:13 +0530

    Time: 7 ms (74.24%), Space: 49.6 MB (72.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,40 @@
 class Solution {
-     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> stringMap = new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) {
         
-        for (String str : strs) {
-            char[] chArray = str.toCharArray();
-            Arrays.sort(chArray);
-            String textkey = String.valueOf(chArray);
-            
-            if (!stringMap.containsKey(textkey)) {
-                stringMap.put(textkey, new ArrayList<>());
+        HashMap<String, List<String>> ansList = new HashMap<String, List<String>>();
+
+        for(int i = 0; i < strs.length; i++){
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+
+            String text = String.valueOf(chars);
+
+            if(!ansList.containsKey(text)){
+                ansList.put(text, new ArrayList<>());
             }
-            stringMap.get(textkey).add(str);
+
+            ansList.get(text).add(strs[i]);
         }
 
-        return new ArrayList<>(stringMap.values());
+        return new ArrayList<>(ansList.values());
+
+
     }
 }
+
+
+
+
+// eat -> {eta, tae, ate, aet, tea, eta}
+// ["eat","tea","tan","ate","nat","bat"]
+
+
+// aet 
+
+
+// {at}
+// a -> 6
+// b -> 1
+// e -> 3
+// n -> 2
+// t -> 6
