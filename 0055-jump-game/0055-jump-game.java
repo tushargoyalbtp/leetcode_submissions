91241791796160c243ab0a8ac7a9aa17c06baefa
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-07-18 07:51:50 +0530

    Time: 2 ms (88.93%), Space: 47.9 MB (63.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,32 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-
-        // for(int i=0; i<nums.length;){
-        //     if(nums[i] == 0 && i<nums.length-1){
-        //         return false;
-        //     }
-
-        //     else if(nums[i] == 0 && i==nums.length-1){
-        //         return true;
-        //     }
-        //     else if(i==nums.length-1){
-        //         return true;
-        //     }
-        //     i = i+nums[i];
-
-        // }
-
-        // return false;
-
         int maxReach = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i > maxReach) {
+        for(int i = 0; i < nums.length; i++){
+            if(i > maxReach){
                 return false;
             }
-            maxReach = Math.max(maxReach, i + nums[i]);
-        }
 
+            maxReach = Math.max(maxReach, i+nums[i]);
+        }
         return true;
     }
 }
