a1958b50550fd90f69edfff8fd4537389305ec6a
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-07-21 21:46:40 +0530

    Time: 1 ms (99.92%), Space: 50.2 MB (56.44%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,72 +1,34 @@
-// class Solution {
-//     public int minPathSum(int[][] grid) {
-//         int m = grid.length;
-//         int n = grid[0].length;
-        
-//         int[][] dp = new int[m][n];
-//         for(int i = 0; i < m; i++){
-//             for(int j = 0; j < n; j++){
-//                 dp[i][j] = -1;
-//             }
-//         }
-
-//         return helper(grid, 0, 0, m, n, dp);
-//     }
-
-//     private int helper(int[][] grid, int row, int col, int m, int n, int[][] dp) {
-       
-//         if (row >= m || col >= n) {
-//             return Integer.MAX_VALUE;
-//         }
-
-//         if (row == m - 1 && col == n - 1) {
-//             return grid[row][col];
-//         }
-
-//         if(dp[row][col] != -1){
-//             return dp[row][col];
-//         }
-
-//         int right = helper(grid, row, col + 1, m, n, dp);
-//         int down = helper(grid, row + 1, col, m, n, dp);
-
-
-//         return dp[row][col] = grid[row][col] + Math.min(right, down);
-//     }
-// }
-
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
+        
+        int row = grid.length;
+        
+        int col = grid[0].length;
+        int[][] dp = new int[row+1][col+1];
 
-       
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
+        for(int[] dpi : dp){
+            Arrays.fill(dpi, Integer.MAX_VALUE);
+        }
+        return helper(grid, 0, 0, row,col, dp);
+    }
 
-       
-                if (i == m - 1 && j == n - 1) {
-                    dp[i][j] = grid[i][j];
-                } 
-              
+    public int helper(int[][] grid, int i, int j , int row, int col, int[][] dp){
 
-                else if (i == m - 1) {
-                    dp[i][j] = grid[i][j] + dp[i][j + 1];
-                } 
-
-
-                else if (j == n - 1) {
-                    dp[i][j] = grid[i][j] + dp[i + 1][j];
-                } 
-                
-                
-                else {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
-                }
-            }
+        if(i< 0 || j < 0 || i >= row || j >= col){
+            return Integer.MAX_VALUE;
         }
 
-        return dp[0][0];
+        
+        if(i == row-1 && j == col-1){
+            return dp[i][j] = grid[i][j];
+        }
+
+        if(dp[i][j] != Integer.MAX_VALUE){
+            return dp[i][j];
+        }
+        dp[i][j] = grid[i][j]+Math.min(helper(grid,i+1,j,row,col, dp),helper(grid,i,j+1,row,col,dp));
+        return dp[i][j];
     }
+
+    
 }
