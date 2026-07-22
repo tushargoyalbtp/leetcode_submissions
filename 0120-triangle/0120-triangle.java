7b37283397fdbdc35672a22790b18eb5a6b8b333
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-07-22 09:37:56 +0530

    Time: 3 ms (80.46%), Space: 45.3 MB (85.32%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,41 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        Integer[][] dp = new Integer[n][n];
+        // int n = triangle.size();
+        // Integer[][] dp = new Integer[n][n];
 
-        return helperMinimum(dp,0,0,triangle);
-    }
+        // return helperMinimum(dp,0,0,triangle);
 
-    public int helperMinimum(Integer[][] dp, int row, int col, List<List<Integer>> triangle){
+                int n = triangle.size();
+        int[] dp = new int[n];
 
-        if (row == triangle.size() - 1) {
-            return triangle.get(row).get(col);
+        // Copy last row
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
         }
 
-        if(dp[row][col]!=null){
-            return dp[row][col];
-        }   
-
-        int dow =  helperMinimum(dp, row+1, col, triangle);
-        int diag = helperMinimum(dp, row+1, col+1, triangle);
-
-        dp[row][col] = triangle.get(row).get(col) + Math.min(dow,diag);
-        return dp[row][col];
+        // Bottom-up
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
     }
+
+    // public int helperMinimum(Integer[][] dp, int row, int col, List<List<Integer>> triangle){
+
+    //     if (row == triangle.size() - 1) {
+    //         return triangle.get(row).get(col);
+    //     }
+
+    //     if(dp[row][col]!=null){
+    //         return dp[row][col];
+    //     }   
+
+    //     int dow =  helperMinimum(dp, row+1, col, triangle);
+    //     int diag = helperMinimum(dp, row+1, col+1, triangle);
+
+    //     dp[row][col] = triangle.get(row).get(col) + Math.min(dow,diag);
+    //     return dp[row][col];
+    // }
 }
