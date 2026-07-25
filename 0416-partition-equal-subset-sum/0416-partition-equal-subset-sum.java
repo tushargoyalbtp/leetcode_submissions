98964f24d5a878b7f155ff91d92bf214196b51c5
Tushar Goyal <tushargoyalbtp2000@gmail.com>
2026-07-25 16:12:34 +0530

    Time: 89 ms (25.49%), Space: 75.9 MB (13.47%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,16 +1,44 @@
 class Solution {
+
+    public boolean canPartition(int[] nums) {
+
+        int totalSum  = 0;
+        for(int i : nums){
+            totalSum += i;
+        }
+
+        if(totalSum%2!=0){
+            return false;
+        }
+
+        int target = totalSum/2;
+
+        int n = nums.length;
+
+        int[][] dp = new int[n][target+1];
+        
+        for(int[] i : dp){
+            Arrays.fill(i,-1);
+        }
+        
+        return functionHelper(n-1, target, dp, nums);
+
+    }
     public boolean functionHelper(int index, int target, int[][] dp, int[] nums){
+
         if(target == 0){
             return true;
         }
+
         if(index == 0){
             return nums[0] == target;
         }
+
         if(dp[index][target] != -1){
             return dp[index][target] == 0 ? false : true;
         }
 
-        boolean nottake = functionHelper(index-1, target, dp, nums);
+        boolean nottake = functionHelper(index-1,target,dp,nums);
 
         boolean take = false;
         if(nums[index] <= target){
@@ -19,59 +47,53 @@ class Solution {
 
         dp[index][target] = nottake || take ? 1 : 0;
 
-        return nottake || take ;
-    }
-    public boolean canPartition(int[] nums) {
-
-        int totalSum = 0;
-        for(int num : nums){
-            totalSum += num;
-        }
-
-        if(totalSum%2 !=0 ){
-            return false;
-        }
-
-        int target =  totalSum / 2;
-        int[][] dp = new int[nums.length][target+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-
-        return functionHelper(nums.length-1, target, dp, nums);
-        
-        // Arrays.sort(nums);
-        
-        // int i = 0;
-        
-        // int j = nums.length-1;
-        
-        // int n = nums.length;
-
-        // return functionHelper(nums, i, 0, j, n);
-    
+        return take || nottake;
     }
 
-    // public boolean functionHelper(int[] nums, int i , int k , int j, int n){
+
+    // public boolean helper(int index, int[] nums, int n){
         
-    //     int presum = 0;
-    //     int aftersum = 0;
+        // if(index > n){
+        //     return false;
+        // }
 
-    //     if( k+1 == n){
-    //         return false;
-    //     }
-        
-    //     for(int p = i; p <= k; p++){
-    //         presum += nums[p];
+        // int sum1 = partitionSum(0,index, nums);
+        // int sum2 = partitionSum(index+1,n,nums);
+
+        // if(sum1==sum2){
+        //     return true;
+        // }
+
+        // else{
+        //     return helper(index+1, nums, n);
+        // }
+
+    // }
+
+    // public int partitionSum(int si, int ei, int[] nums){
+    //     int sum = 0;
+    //     for(int i = si; i<=ei; i++){
+    //         sum += nums[i];
     //     }
 
-    //     for(int q = k+1; q <= j; q++){
-    //         aftersum += nums[q];
-    //     }
-
-    //     if(presum == aftersum){
-    //         return true;
-    //     } 
-    //     return functionHelper(nums, i, k+1, j, n);
+    //     return sum;
     // }
 }
+
+
+
+// totalsum = 22
+
+// [1,5,11,5]
+
+// 1<22, i = 0;
+// 22-1 -> 21,
+
+// i = 1, 5<21
+// 21-5 = 16
+
+// i = 2, 11<16
+// 16-11 = 5
+
+// i = 3, 5<=5
+// 5-5 = 0;
